1b356833a0fffcc904e26ec1c1095002ede711f6
Arsen Sultanov <sultanov.a.i13@gmail.com>
2017-12-18 00:47:49 +0300

    begin

---
+++ minerLogic.js
@@ -0,0 +1,69 @@
+var minerLogic = (function(){
+    var mines=[];
+    var app = document.getElementById("app");
+
+    function  getRandomArbitrary(lenght){
+        return Math.floor(Math.random() * (lenght - 0) + 0);
+    }
+
+
+    function tableDataClickEvent(){
+        if(isMine(this)){
+            this.style.backgroundColor = 'red';
+            return;
+        }
+        this.style.backgroundColor = 'blue';
+    };
+    
+    function tableDataContextMenuClickEvent(){
+        this.style.backgroundColor = 'green'        
+    };
+
+
+    function isMine(TableData){
+        for(var isMineIteration = 0; isMineIteration < mines.length; isMineIteration++){       
+            if(TableData.id == mines[isMineIteration].id){
+                return true;
+            }
+        }
+    };
+
+
+    function addMines(){
+        var tableDatas = document.getElementsByTagName("td");
+       for(var addMinesIteration = 0; addMinesIteration < 5; addMinesIteration++){
+        mines.push(
+            tableDatas[getRandomArbitrary(tableDatas.length)]
+        );
+       }
+    }
+
+
+    function createTable(size){
+        var table = document.createElement('table');
+        var tmp = "";
+        for(var row = 0; row < size; row++){
+            tmp += '<tr>';
+            for(var colum = 0; colum < size; colum++){
+                tmp += '<td id="' + row + ':' + colum + '">'+ row + ':' + colum + '</th>'
+            }
+            tmp += '</tr>';
+        }
+        table.innerHTML += tmp;
+        return table;
+    }
+
+
+    function addEventListenerForTable(){
+        var tableDatas = document.getElementsByTagName("td");
+        for(var tdIteration = 0; tdIteration < tableDatas.length; tdIteration++){
+            tableDatas[tdIteration].addEventListener('click', tableDataClickEvent);
+            tableDatas[tdIteration].addEventListener('contextmenu', tableDataContextMenuClickEvent);
+        };
+    }
+    
+    app.appendChild(createTable(7));
+    addEventListenerForTable();
+    addMines();
+    
+})()
